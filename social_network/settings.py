584b8946f066c4e4f4f3b3d922c14dc3545c47fe
andrey <polyakovv.andrey.01@gmail.com>
2023-11-24 09:29:12 +0200

    Add middleware to monitor user activity

--- social_network/settings.py
+++ social_network/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.authentication.middleware.LastActivityMiddleware',
 ]
 
 ROOT_URLCONF = 'social_network.urls'
