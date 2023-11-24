5dd6ea0871261eaa8f7ae13fcc147d8985ba5b72
andrey <polyakovv.andrey.01@gmail.com>
2023-11-24 08:32:29 +0200

    added models for like and dislike

--- apps/post/models.py
+++ apps/post/models.py
@@ -13,3 +13,20 @@ class Post(models.Model):
     def __str__(self):
         return self.text
 
+
+class PostLike(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name='user_like', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='liked_post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Post {self.post.id} was liked by {self.user.name}"
+
+
+class PostDislike(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name='user_disliked', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='disliked_post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Post {self.post.id} was disliked by {self.user.name}"
